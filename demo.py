21f6ebb86b8d6f1e602276e585692818683c39c7
Amey Joshi <ameyjoshi2001rj@gmail.com>
2021-06-10 16:13:12 +0530

    update

--- demo.py
+++ demo.py
@@ -11,3 +11,10 @@ print(len(list))
 
 # ()--> represents tupple
 # []-->represents list
+
+for number in 1,2,3,4,5:
+     print("The current number is ",number)
+
+
+for number in range (1, 10, 2):
+      print("The current number is \n ", number)
