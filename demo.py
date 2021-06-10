2183026336f843e7f41a7d072cf61c8676461fdb
Amey Joshi <ameyjoshi2001rj@gmail.com>
2021-06-10 12:09:24 +0530

    update

---
+++ demo.py
@@ -0,0 +1,13 @@
+print("Hi! This amey!")
+print("Use Shift+Enter to get output on terminal")
+
+list = [10,20,20]
+list.append(30)
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(len(list))
+
+# ()--> represents tupple
+# []-->represents list
